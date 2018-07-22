d70ddb63a4e47c62d07b16b88273a30f2dc789ea
Matthew Soh <matthewsoh.cw@gmail.com>
2018-07-22 01:16:17 -0700

    Change class abstraction to Instance instead of Client

--- roger/roger.py
+++ roger/roger.py
@@ -23,16 +23,15 @@ DEFAULT_INSTANCE_PARAMETERS = {
         }
 }
 
-class Client:
+class Instance:
     def __init__(self):
-        self.instances = {}
         self.ec2 = boto.resource('ec2')
         pass
 
     def spinup(self, *, code_path, data_path):
         pass
 
-    def load_instance(self, *, key, days=3, max_price ='0.0035', parameters={}):
+    def load_instance(self, *, days=3, max_price ='0.0035', parameters={}):
         instance_parameters = copy.deepcopy(DEFAULT_INSTANCE_PARAMETERS)
         spot_options = instance_parameters['InstanceMarketOptions']['SpotOptions']
         spot_options['ValidUntil'] = days_from_now(days)
@@ -41,22 +40,19 @@ class Client:
 
         verify()
 
-        instance = self.ec2.create_instances(**instance_parameters)
-        assert (key not in self.instances)
-        self.instances[key] = instance
+        assert instance_parameters['MaxCount'] == 1
+        assert instance_parameters['MinCount'] == 1
+        self.instance = self.ec2.create_instances(**instance_parameters)[0]
+        self._connect_to_instance()
 
-        return instance
 
-    def load_code(self, *, instance=None, key=None, code_path, remote=False):
+    def load_code(self, *, code_path, remote_path, remote=False):
         pass
 
-    def load_data(self, *, instance=None, key=None, data_path, remote=False):
+    def load_data(self, *, data_path, remote_path, remote=False):
         pass
 
     # Helpers
-    def _get_instance(self, key):
-        assert (key in self.instances), "Instance not found"
-        return self.instances[key]
 
 
 def verify():
