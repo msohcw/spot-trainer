e8d91c5806a44645ea50246a9c56942ea4ae16a8
Matthew Soh <matthewsoh.cw@gmail.com>
2018-08-06 23:06:37 -0700

    Blacken code

--- dalmatian/dalmatian.py
+++ dalmatian/dalmatian.py
@@ -4,26 +4,25 @@ import pickle
 import copy
 
 ###### For testing ######
-DEFAULT_INSTANCE_NAME = 'amazing-artichoke'
+DEFAULT_INSTANCE_NAME = "amazing-artichoke"
 
 #########################
 
 
 instance = None
-s3 = boto.resource('s3')
-bucket = s3.Bucket('dalmatian')
+s3 = boto.resource("s3")
+bucket = s3.Bucket("dalmatian")
 watch_set = {}
+
 
 class Instance:
     def __init__(self, instance_name):
         self.name = instance_name
-        self.s3_client = boto.client('s3')
-        self.ec2_client = boto.client('ec2')
+        self.s3_client = boto.client("s3")
+        self.ec2_client = boto.client("ec2")
 
         try:
-            self.state = {
-                'parameters': {}
-            }
+            self.state = {"parameters": {}}
             self._initialize_storage()
             self.storage_initialized = True
         except Exception as e:
@@ -35,12 +34,14 @@ class Instance:
         self.state_name = "{}-state".format(self.name)
 
         # Check if data already exists
-        state_keys = [state.key for state in bucket.objects.filter(Prefix=self.state_name)]
+        state_keys = [
+            state.key for state in bucket.objects.filter(Prefix=self.state_name)
+        ]
 
         if self.state_name in state_keys:
             _log("Prior state found, loading state")
             self._safe_retry(self._get_state)
-        else: # we need to initialize the state object
+        else:  # we need to initialize the state object
             _log("Initializing remote state")
             self._safe_retry(self._put_state)
 
@@ -51,34 +52,35 @@ class Instance:
 
     def _get_state(self):
         _log("Requesting state from S3")
-        response = self.s3_client.get_object(Bucket=bucket.name,
-                                             Key=self.state_name)
+        response = self.s3_client.get_object(Bucket=bucket.name, Key=self.state_name)
 
-        status = response['ResponseMetadata']['HTTPStatusCode']
+        status = response["ResponseMetadata"]["HTTPStatusCode"]
         if status != 200:
             _log("Request failed")
             return False
 
-        stream = response['Body']
+        stream = response["Body"]
         bytedata = stream.read()
         try:
             self.state = pickle.loads(bytedata)
         except ModuleNotFoundError as e:
             _log(e.msg)
-            _log("A module used to save the data is missing and the data cannot"
-                 " be loaded. Check if the required module is installed in your"
-                 " local environment.")
+            _log(
+                "A module used to save the data is missing and the data cannot"
+                " be loaded. Check if the required module is installed in your"
+                " local environment."
+            )
             raise e
         _log("Request succeeded")
         return True
 
     def _put_state(self):
         _log("Storing state into S3")
-        response = self.s3_client.put_object(Bucket=bucket.name,
-                                             Key=self.state_name,
-                                             Body=self._bytedata())
+        response = self.s3_client.put_object(
+            Bucket=bucket.name, Key=self.state_name, Body=self._bytedata()
+        )
 
-        status = response['ResponseMetadata']['HTTPStatusCode']
+        status = response["ResponseMetadata"]["HTTPStatusCode"]
         if status != 200:
             _log("Storage failed")
             return False
@@ -87,10 +89,9 @@ class Instance:
 
     def _erase_state(self):
         _log("Erasing state from S3")
-        response = self.s3_client.delete_object(Bucket=bucket.name,
-                                                Key=self.state_name)
+        response = self.s3_client.delete_object(Bucket=bucket.name, Key=self.state_name)
 
-        status = response['ResponseMetadata']['HTTPStatusCode']
+        status = response["ResponseMetadata"]["HTTPStatusCode"]
         if status != 204:
             _log("Erasure failed")
             return False
@@ -111,40 +112,48 @@ class Instance:
     def erase(self):
         self._safe_retry(self._erase_state)
 
+
 def _log(message):
     print(message)
 
+
 def _preflight_checks(storage=True):
-    assert instance != None, (
-           "No instance found, have you run dalmatian.setup() yet?")
+    assert instance != None, "No instance found, have you run dalmatian.setup() yet?"
     if storage:
-        assert instance.storage_initialized, (
-               "Storage not initialized. Retry dalmatian.setup()")
+        assert (
+            instance.storage_initialized
+        ), "Storage not initialized. Retry dalmatian.setup()"
+
 
 ### Public Interface ###
 
+
 def setup():
     global instance
-    instance_name = (os.environ.get('DALMATIAN_INSTANCE')
-                    or DEFAULT_INSTANCE_NAME)
+    instance_name = os.environ.get("DALMATIAN_INSTANCE") or DEFAULT_INSTANCE_NAME
     _log("Beginning setup")
     instance = Instance(instance_name)
     _log("Setup complete")
 
+
 def store_params(d):
     _preflight_checks()
-    instance.state['parameters'].update(d)
+    instance.state["parameters"].update(d)
     return get_params()
+
 
 def store_param(key, value):
     store_params({key: value})
 
+
 def get_params():
     _preflight_checks()
-    return copy.deepcopy(instance.state['parameters'])
+    return copy.deepcopy(instance.state["parameters"])
+
 
 def get_param(key, default=None):
-    return instance.state['parameters'].get(key, default)
+    return instance.state["parameters"].get(key, default)
+
 
 class ImmutableObjectException(Exception):
     def __init__(self, item):
@@ -153,7 +162,10 @@ class ImmutableObjectException(Exception):
     def __str__(self):
         return """ Immutable object given of type {}. This will not change and
                    not useful to watch. If you really need to store a constant,
-                   use store_params instead. """.format(self.type_name)
+                   use store_params instead. """.format(
+            self.type_name
+        )
+
 
 def watch_param(key, value):
     _preflight_checks()
@@ -162,9 +174,11 @@ def watch_param(key, value):
     else:
         watch_set[key] = value
 
+
 def checkpoint():
     _preflight_checks()
     instance.save()
+
 
 def wipe():
     # TODO This is a temporary way of handling the need to erase past training
@@ -178,6 +192,7 @@ def wipe():
     instance.erase()
     instance = None
     _log("Wipe complete")
+
 
 def terminate():
     # This is a stubbed method that could be used to do self-termination for
--- roger/roger.py
+++ roger/roger.py
@@ -22,6 +22,7 @@ interactive Python REPL.
 
 original_send_interrupt = fabric.runners.Remote.send_interrupt
 
+
 def send_interrupt(self, interrupt):
     if not self.remote_interrupt:
         self.remote_interrupt = True
@@ -29,8 +30,10 @@ def send_interrupt(self, interrupt):
     else:
         original_send_interrupt(self, interrupt)
 
+
 def set_remote_interrupt(self, value):
-    self.config.runners['remote'].remote_interrupt = value
+    self.config.runners["remote"].remote_interrupt = value
+
 
 fabric.runners.Remote.send_interrupt = send_interrupt
 fabric.Connection.set_remote_interrupt = set_remote_interrupt
@@ -38,39 +41,43 @@ fabric.Connection.set_remote_interrupt = set_remote_interrupt
 ##################################
 
 DEFAULT_INSTANCE_PARAMETERS = {
-    'ImageId': 'ami-c47c28bc',
-    'MinCount': 1,
-    'MaxCount': 1,
+    "ImageId": "ami-c47c28bc",
+    "MinCount": 1,
+    "MaxCount": 1,
     ### TEMPORARY PARAMETERS ###
-    'InstanceType': 't2.micro',
-    'SubnetId': 'subnet-338dc669',
-    'KeyName': 'ml-ec2-generic',
+    "InstanceType": "t2.micro",
+    "SubnetId": "subnet-338dc669",
+    "KeyName": "ml-ec2-generic",
     ############################
-    'InstanceMarketOptions': {
-        'MarketType': 'spot',
-        'SpotOptions': {
-            'SpotInstanceType': 'persistent',
-            'ValidUntil': None,
-            'MaxPrice': None,
-            'InstanceInterruptionBehavior': 'stop'
-            }
-        }
+    "InstanceMarketOptions": {
+        "MarketType": "spot",
+        "SpotOptions": {
+            "SpotInstanceType": "persistent",
+            "ValidUntil": None,
+            "MaxPrice": None,
+            "InstanceInterruptionBehavior": "stop",
+        },
+    },
 }
 
-DEFAULT_SECURITY_GROUP = 'us-west-2-default-sg'
-DEFAULT_KEY_PAIR = (pathlib.Path('~/.ssh/ml-ec2-generic.pem')
-                        .expanduser().as_posix())
+DEFAULT_SECURITY_GROUP = "us-west-2-default-sg"
+DEFAULT_KEY_PAIR = pathlib.Path("~/.ssh/ml-ec2-generic.pem").expanduser().as_posix()
+
 
 class Instance:
     def __init__(self):
-        self.ec2 = boto.resource('ec2')
-        self.security_group_ids = self.ec2.security_groups.all().filter(
-                Filters=[{'Name': 'group-name',
-                          'Values': (DEFAULT_SECURITY_GROUP,)}]).limit(1)
+        self.ec2 = boto.resource("ec2")
+        self.security_group_ids = (
+            self.ec2.security_groups.all()
+            .filter(
+                Filters=[{"Name": "group-name", "Values": (DEFAULT_SECURITY_GROUP,)}]
+            )
+            .limit(1)
+        )
         self.security_group_ids = [x.id for x in self.security_group_ids]
-        self.remote_code_path = '/home/ubuntu/code.py'
-        self.remote_data_path = '/not/yet/implemented/data'
-        self.conda_env = 'pytorch_p36'
+        self.remote_code_path = "/home/ubuntu/code.py"
+        self.remote_data_path = "/not/yet/implemented/data"
+        self.conda_env = "pytorch_p36"
         pass
 
     def spinup(self, *, code_path, data_path):
@@ -80,18 +87,18 @@ class Instance:
         self.setup_instance()
         self.run()
 
-    def load_instance(self, *, days=3, max_price ='0.0035', parameters={}):
+    def load_instance(self, *, days=3, max_price="0.0035", parameters={}):
         instance_parameters = copy.deepcopy(DEFAULT_INSTANCE_PARAMETERS)
-        spot_options = instance_parameters['InstanceMarketOptions']['SpotOptions']
-        spot_options['ValidUntil'] = days_from_now(days)
-        spot_options['MaxPrice'] = max_price
-        instance_parameters['SecurityGroupIds'] = self.security_group_ids
+        spot_options = instance_parameters["InstanceMarketOptions"]["SpotOptions"]
+        spot_options["ValidUntil"] = days_from_now(days)
+        spot_options["MaxPrice"] = max_price
+        instance_parameters["SecurityGroupIds"] = self.security_group_ids
         instance_parameters.update(parameters)
 
         verify()
 
-        assert instance_parameters['MaxCount'] == 1
-        assert instance_parameters['MinCount'] == 1
+        assert instance_parameters["MaxCount"] == 1
+        assert instance_parameters["MinCount"] == 1
         self.instance = self.ec2.create_instances(**instance_parameters)[0]
         self._connect_to_instance()
 
@@ -99,26 +106,28 @@ class Instance:
         # HACK ~/.bashrc doesn't get run by Fabric commands so we need to copy
         # it manually to ~/.profile. TODO find a better way to do this
         log("Beginning instance setup")
-        self.connection.run('cat ~/.bashrc | grep export >> ~/.profile')
+        self.connection.run("cat ~/.bashrc | grep export >> ~/.profile")
         log("Instance setup complete")
 
     def load_code(self, *, code_path, remote_path, remote=False):
-        if not code_path: return
+        if not code_path:
+            return
         if not remote:
             self._load_local_code(code_path)
 
     def load_data(self, *, data_path, remote_path, remote=False):
-        if not data_path: return
+        if not data_path:
+            return
 
     def run(self):
         log("Initiating screen job")
         # This repeated prefixing is kinda hacky. TODO look into better ways of
         # getting persistent context/environment
-        with self.connection.prefix('source ~/.profile'):
-            with self.connection.prefix(
-                    'source activate {}'.format(self.conda_env)):
-                self.connection.run('screen -dmL python -u {}'.format(
-                    self.remote_code_path))
+        with self.connection.prefix("source ~/.profile"):
+            with self.connection.prefix("source activate {}".format(self.conda_env)):
+                self.connection.run(
+                    "screen -dmL python -u {}".format(self.remote_code_path)
+                )
         log("Screen job initiated")
 
     def listen(self):
@@ -133,21 +142,21 @@ class Instance:
         while True:
             self.instance.reload()
             public_ip = self.instance.public_ip_address
-            if public_ip: break
+            if public_ip:
+                break
             log("Waiting for {} seconds for instance IP address".format(backoff))
             time.sleep(backoff)
             backoff = min(30, backoff + 5)
         log("IP address obtained:", public_ip)
 
         port = 22
-        user = 'ubuntu'
-        self.connection = Connection(host=public_ip,
-                                     user=user,
-                                     port=port,
-                                     connect_kwargs={
-                                         'key_filename': DEFAULT_KEY_PAIR
-                                         }
-                                     )
+        user = "ubuntu"
+        self.connection = Connection(
+            host=public_ip,
+            user=user,
+            port=port,
+            connect_kwargs={"key_filename": DEFAULT_KEY_PAIR},
+        )
         self.connection.set_remote_interrupt(True)
         log("Waiting for instance to be in 'running' mode")
         self.instance.wait_until_running()
@@ -168,21 +177,26 @@ class Instance:
         self.connection.put(str(abs_path), remote=self.remote_code_path)
         log("Loading code complete")
 
+
 class LogReader(StreamWatcher):
     def __init__(self, file_path=None):
         pass
+
     def submit(self, stream):
         print(stream)
         return []
+
 
 def verify():
     # stubbed method to do verification
     pass
 
+
 def days_from_now(x):
     now = datetime.datetime.now()
     delta = datetime.timedelta(x)
     return now + delta
+
 
 def log(*args):
     print(*args)
